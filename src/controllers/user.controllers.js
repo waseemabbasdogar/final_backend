69ed3c3e7df34a3ff1a7f6a1859fceb2e78fc8fc
waseemabbasdogar <wassim3451@gmail.com>
2025-04-16 08:07:45 +0500

    two steps of register user complete

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,37 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { apiError } from '../utils/apiError.js';
+
+// const registerUser = asyncHandler( async (req, res) => {
+//     res.status(200).json({
+//         message : "ok"
+//     })
+// } );
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message : "ok"
-    })
+    // get user details from frontend
+    // validation (not empty)
+    // check user already exist by username or email
+    // check for images (check for avatar)
+    // upload them on cloudinay (avatar)
+    // create user Object -- create entry in db
+    // remove password and fresh token fields from response.
+    // check for user creation 
+    // return response
+
+    const {fullName, username, email, password} = req.body;
+    console.log("email", email);
+    
+    // validation of fields.
+
+    // if(fullName === ""){
+    //     throw new apiError(400, 'full name is required.')
+    // };
+    if(
+        [fullName, email, password, username].some((field) => field?.trim() === "")
+    ){
+        throw new apiError(400, 'all fields are required.')
+    }
+
 } );
 
 export {registerUser}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,24 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controllers.js";
 
+import { upload } from "../middlewares/multer.middlewares.js";
+
 const router = Router();
 
-router.route('/register').post(registerUser)
+
+// get user details from frontend.
+router.route('/register').post(
+    upload.fields([
+        {
+            name : "avatar",
+            maxCount : 1
+        },
+        {
+            name : "coverImage",
+            maxCount : 2
+        }
+    ]),
+    registerUser)
 
 
 export default router
